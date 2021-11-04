78b099c8e240952caf5ce01c6d1023adeef08517
calwong23 <calvinwong.tse@gmail.com>
2021-11-03 23:15:55 -0700

    webpack.config.js added sass-loader to rules

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         rules: [
             {
                 test:/\.s?css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", 'sass-loader'],
             },
             {
                 test: /\.js$/,
